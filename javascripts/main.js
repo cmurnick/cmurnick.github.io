c324571cf03302cb99f642bafbc98d95d6094dff
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-11-12 20:26:27 -0600

    all data pulls from firebase and writes to dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const apiKey = require('./apiKey');
-const project = require('./project');
-const blog = require('./blog');
+// const project = require('./project');
+// const blog = require('./blog');
+const data = require('./data');
 
 apiKey.retrieveKeys();
+
 
 
 
--- javascripts/project.js
+++ javascripts/project.js
@@ -1,9 +1,53 @@
 "use strict";
 
-let firebaseKey;
+let projectKey;
 
 const setKey = (apiKey) => {
-  firebaseKey = apiKey;
+  projectKey = apiKey;
+  console.log ("project key", projectKey);
 };
 
-module.exports = {setKey};
+
+
+const getProjects = (project) => {
+	let productzString ='';
+
+	for (let i = 0; i < project.length; i++) {
+
+
+		let domString = "";
+
+		domString += `<div class= "product3 col-xs-4 blogContainer">`;
+		domString +=	`<div class="product2 set-height thumbnail project">`;
+		domString +=		'<div class="product1 caption">';
+		domString +=			`<div class="text-uppercase list title"> ${project[i].name} </div>`;
+		domString +=			`<div class ="list date"> ${project[i].description} </div>`;
+		domString +=			`<div class ="list screensot"> ${project[i].screenshot} </div>`;
+		domString +=			`<div class ="list link"> ${project[i].link} </div>`;
+		domString +=			`<div class ="list deployLink"> ${project[i].deployLink} </div>`;
+
+		domString +=		`</div>`;
+		domString += 	`</div>`;
+		domString += `</div>`;
+
+
+
+		productzString += domString;
+		}
+
+		printToDom(productzString);
+	};
+
+const printToDom = (strang) => {
+	$('#project-holder').append(strang);
+};
+
+
+let selectedBlog;
+
+
+// $('body').on('click', '.project', (event) => {
+// 	$(event.target).closest(".project").appendTo('#entireBlog');
+// });
+
+module.exports = {setKey, getProjects};
